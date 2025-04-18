3fc8ca15cd478659a3ab0fa36747655e43241f24
profdupuis <96577486+profdupuis@users.noreply.github.com>
2025-04-18 11:34:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -130,6 +130,5 @@ def reset_session():
 
 # Lancement de l'application Flask en mode debug
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
